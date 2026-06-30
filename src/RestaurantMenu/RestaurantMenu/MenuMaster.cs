cea9ca304577f1f10a13bfbe254ea48fb2cff5a4
GreatSherhebe1 <AkinavaTor@yandex.ru>
2026-06-30 14:04:06 +0500

    ADD: MenuMaster ctor + empty methods

--- src/RestaurantMenu/RestaurantMenu/MenuMaster.cs
+++ src/RestaurantMenu/RestaurantMenu/MenuMaster.cs
@@ -1,7 +1,46 @@
 ﻿namespace RestaurantMenu
 {
-    public class Class1
+    public class MenuMaster
     {
+        private int dishesPerPage;
+        private readonly List<string> dishes;
+        private int winterStartIndex;
 
+        public MenuMaster(IEnumerable<string> commonMenuDishes, IEnumerable<string> winterMenuDishes, int dishesPerPage)
+        {
+            var commonMenu = commonMenu?.ToList() ?? new List<string>();
+            var winterMenu = winterMenuDishes?.ToList() ?? new List<string>();
+
+            if (commonMenu.Count() + winterMenu.Count() < 1)
+                throw new ArgumentException("No dishes defined");
+
+            if (dishesPerPage < 1)
+                throw new ArgumentException("Dishes per page can't be less then 1");
+
+            dishes = new List<string>();
+            dishes.AddRange(commonMenu);
+            dishes.AddRange(winterMenu);
+
+            if (winterMenu.Count > 0)
+            {
+                var commonMenuLastPage = (int)Math.Ceiling(commonMenu.Count / dishesPerPage);
+                var commonMenuModulo = commonMenu.Count % dishesPerPage;
+                winterStartIndex = commonMenuModulo == 0 ? commonMenuLastPage + 1 : commonMenuLastPage;
+            }
+            else
+                winterStartIndex = -1;
+        }
+
+        public int GetWinterMenuFirstPage() => winterStartIndex;
+
+        public int GetDishesNumber();
+
+        public int GetPagesNumber();
+
+        public int GetDishesNumberOnPage(int page);
+
+        public IEnumerable<string> GetDishesOnPage(int page);
+
+        public IEnumerable<string> GetFirstDisesOnPages();
     }
 }
